0a3b0ec1fddcfb6edb326d1cda293a03ec173cf0
DeexantBharti <deexantbharti4321@gmail.com>
2025-03-05 20:09:50 +0530

    df

--- public/client.js
+++ public/client.js
@@ -33,7 +33,7 @@ function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
     let className = type
     mainDiv.classList.add(className, 'message')
-
+    
     let markup = `
         <h4>${msg.user}</h4>
         <p>${msg.message}</p>
