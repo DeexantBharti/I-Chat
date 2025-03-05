3550e320ad1984b41a74e840a72600d8f6f3ba6d
DeexantBharti <deexantbharti4321@gmail.com>
2025-03-05 19:04:02 +0530

    meesage

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
@@ -17,38 +16,10 @@ app.get('/', (req, res) => {
 
 // Socket 
 const io = require('socket.io')(http)
-     
+
 io.on('connection', (socket) => {
     console.log('Connected...')
     socket.on('message', (msg) => {
         socket.broadcast.emit('message', msg)
     })
-
-=======
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-
-const PORT = process.env.PORT || 3000
-
-http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
-
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-// Socket 
-const io = require('socket.io')(http)
-     
-io.on('connection', (socket) => {
-    console.log('Connected...')
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
-
->>>>>>> 226869e (This is chat app with socket Io)
 })
